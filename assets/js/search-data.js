a069b6c75beec46f94aaf34e1f7f08d004e43c79
cenweizhang <159774175+cenweizhang@users.noreply.github.com>
2026-02-26 15:22:11 +0000

    Deploying to gh-pages from @ cenweizhang/cenweizhang.github.io@00cc38774dd8c528c7fb4380faf07c45b45d647f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -82,34 +82,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%63%77%7A%68%61%6E%67%32%30%30%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/cenweizhang", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/岑蔚-张-6a0871354", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
